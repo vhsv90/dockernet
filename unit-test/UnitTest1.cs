d13eed770b4cf5eb9c3fa344fdd6c8a7ab2957ff
Victor Sanchez <vhsv90@gmail.com>
2019-05-29 14:52:11 -0600

    UT project added

---
+++ unit-test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace unit_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
